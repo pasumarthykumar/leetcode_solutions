68fb8e61832ba03e71e73a2d99cb1448e40b65c4
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2024-12-28 13:29:47 -0800

    Valid Palindrome Solution

---
+++ src/com/twopointers/ValidPalindrome.java
@@ -0,0 +1,23 @@
+package com.twopointers;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        String ms="";
+        for(int k=0;k<s.length();k++){
+            if(Character.isLetterOrDigit(s.charAt(k))){
+                ms=ms+s.charAt(k);
+            }
+        }
+        ms=ms.toLowerCase();
+        int j=ms.length()-1;
+        while(i<j){
+            if(ms.charAt(i)!=ms.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
