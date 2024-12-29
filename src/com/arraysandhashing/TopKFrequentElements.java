f85d0b36171e09784f35ae5bd78b74295bed169e
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2024-12-29 12:40:26 -0800

    Complexities added

---
+++ src/com/arraysandhashing/TopKFrequentElements.java
@@ -0,0 +1,27 @@
+package com.arraysandhashing;
+import java.util.*;
+
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hp = new HashMap<>();
+        int a[] = new int[k];
+
+        // Build frequency map
+        for (int num : nums) {
+            hp.put(num, hp.getOrDefault(num, 0) + 1);
+        }
+
+        // Create a list of Map.Entry (element, frequency) for sorting
+        ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>(hp.entrySet());
+
+        // Sort by frequency in descending order
+        list.sort((entry1, entry2) -> entry2.getValue() - entry1.getValue());
+
+        // Collect the top k frequent elements
+        for (int i = 0; i < k; i++) {
+            a[i] = list.get(i).getKey();
+        }
+
+        return a;
+    }
+}
