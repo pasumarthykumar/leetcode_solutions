691ee8103959a94f692b2b7516725705da950e16
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2025-02-06 15:56:58 -0800

    Generate Parentheses

---
+++ src/backtracking/GenerateParentheses.java
@@ -0,0 +1,24 @@
+package backtracking;
+import java.util.*;
+
+public class GenerateParentheses {
+
+	    public List<String> generateParenthesis(int n) {
+	        List<String> al=new ArrayList<>();
+	        generateParenthesis(al,n,0,0,"");
+	        return al;
+	    }
+	    public void generateParenthesis(List<String> al,int n,int openCount,int closeCount,String str){
+	        if(openCount==closeCount && openCount==n){
+	            al.add(str);
+	            return;
+	        }
+	        if(openCount<n){
+	            generateParenthesis(al,n,openCount+1,closeCount,str+"(");
+	        }
+	        if(closeCount<openCount){
+	            generateParenthesis(al,n,openCount,closeCount+1,str+")");
+	        }
+	        return;
+	    }
+	}
